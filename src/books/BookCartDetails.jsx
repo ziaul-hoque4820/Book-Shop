c9716c624cf72269ce40b74d8b59a33fa25220b0
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-05-28 23:06:24 +0600

    BookCartDetails Functional work Implement

--- src/books/BookCartDetails.jsx
+++ src/books/BookCartDetails.jsx
@@ -3,7 +3,19 @@ import { BookContext } from "../context";
 import { getImageUrl } from "../utils/bookImage";
 
 const BookCartDetails = ({ onClose }) => {
-    const {cartData} = useContext(BookContext);
+    const { cartData, setCartData } = useContext(BookContext);
+
+    const handleDeleteBook = (event, itemId) => {
+        event.preventDefault();
+
+        const filteredItem = cartData.filter(item => {
+            return item.id !== itemId;
+        });
+
+        setCartData([...filteredItem]);
+
+    };
+
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm backdrop-brightness-75 bg-black/10 px-4">
             <div className="bg-gray-500 text-white p-6 rounded-xl w-full max-w-5xl mx-auto relative">
@@ -20,27 +32,29 @@ const BookCartDetails = ({ onClose }) => {
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
                     {/* Cart List Section */}
                     <div className="md:col-span-3 space-y-4">
-                        {cartData.map(item => (
-                            <div key={item.id} className="flex justify-between items-center bg-gray-800 p-4 rounded-lg">
-                                <div className="flex items-center gap-4">
-                                    <img src={getImageUrl(item.image)} alt={item.name} className="w-16 h-20 rounded" />
-                                    <div>
-                                        <h3 className="font-medium text-base">{item.name}</h3>
-                                        <p className="text-sm text-gray-400">{item.author}</p>
+                        {cartData.length === 0
+                            ? (<p className="text-center text-3xl font-bold ">Your Cart Is empty</p>)
+                            : (cartData.map(item => (
+                                <div key={item.id} className="flex justify-between items-center bg-gray-800 p-4 rounded-lg">
+                                    <div className="flex items-center gap-4">
+                                        <img src={getImageUrl(item.image)} alt={item.name} className="w-16 h-20 rounded" />
+                                        <div>
+                                            <h3 className="font-medium text-base">{item.name}</h3>
+                                            <p className="text-sm text-gray-400">{item.author}</p>
+                                        </div>
                                     </div>
+                                    <div className="text-sm">${item.price}</div>
+                                    <div className="flex items-center gap-2 border rounded-md px-2 py-1">
+                                        <button className="px-2 text-lg">−</button>
+                                        <span>2</span>
+                                        <button className="px-2 text-lg">+</button>
+                                    </div>
+                                    <div className="text-sm">${item.price}</div>
+                                    <button onClick={(e) => handleDeleteBook(e, item.id)} className="text-red-400 hover:text-red-600">
+                                        🗑️
+                                    </button>
                                 </div>
-                                <div className="text-sm">${item.price}</div>
-                                <div className="flex items-center gap-2 border rounded-md px-2 py-1">
-                                    <button className="px-2 text-lg">−</button>
-                                    <span>2</span>
-                                    <button className="px-2 text-lg">+</button>
-                                </div>
-                                <div className="text-sm">${item.price}</div>
-                                <button className="text-red-400 hover:text-red-600">
-                                    🗑️
-                                </button>
-                            </div>
-                        ))}
+                            )))}
                     </div>
 
                     {/* Order Summary */}
